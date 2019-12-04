26010f1d0a07940a0be39ff1870c4db3bb1d297e
Harman <Harman007@Ramans-iMac.local>
2019-12-04 15:25:46 -0500

    Added files

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+ // https://dribbble.com/shots/2955075-Neon-Sign-in-CSS
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+ // https://dribbble.com/shots/2955075-Neon-Sign-in-CSS
